72fa25e30a1368b436c1cdb4ac50722e0f593946
Griffin Li <griffin@griffinli.com>
2023-08-21 20:29:29 -0400

    Refactor codebase and add error handling

---
+++ app/content.tsx
@@ -0,0 +1,106 @@
+import {useEffect, useState} from "react";
+import {useCompletion} from "ai/react";
+import {useWidgetApi} from "@beeper/matrix-widget-toolkit-react";
+import {RoomAccountData, RoomEvent} from "@beeper/matrix-widget-toolkit-api";
+import {getDisplayNameData, Message, processMessages, RoomMessageEvent} from "@/app/helpers";
+
+function generatePrompt(messages: Message[], displayNameData: Record<string, string>) {
+    const start_prompt = "Here is a transcript of a chat:"
+
+    const end_prompt = "Give a bullet-point summary that is detailed, thorough, and that accurately captures the conversation. Include names only to tell me who's backing up a claim or assertion. After reading your summary, my understanding of what happened should be as good as if I had read the messages myself. Give me details and specifics. Use active voice throughout. Only include links that would be genuinely useful for me to have. Write only the summary, without including text like \"Here\'s the summary\" or \"I hope this helped\""
+    let messages_prompt = "";
+
+    messages.forEach((message) => {
+        let username = displayNameData[message.user] || message.user;
+        messages_prompt += `\n${username}: ${message.content}`;
+    })
+
+    return `${start_prompt}\n\n${messages_prompt}\n\n${end_prompt}`;
+}
+
+export default function Content() {
+    const [noMessages, setNoMessages] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [summarizing, setSummarizing] = useState(false);
+    const [count, setCount] = useState(250);
+    const [messageCount, setMessageCount] = useState(0);
+    const [error, setError] = useState("");
+
+    const { complete, completion } = useCompletion({
+        api: '/api/completion',
+        onResponse: () => {
+            setLoading(false);
+            setSummarizing(true);
+        },
+        onError: (error) => {
+            setError(error.message);
+        }
+    })
+    const widgetApi = useWidgetApi();
+
+    async function fetchData(useUnread: boolean, limit: number = 500) {
+
+        let roomEvents: RoomEvent<RoomMessageEvent>[];
+
+        if (useUnread) {
+            const fullyReadData: RoomAccountData<any>[] = await widgetApi.receiveRoomAccountData('m.fully_read');
+            const fullyRead: string | undefined = fullyReadData[0].content.event_id;
+            roomEvents = await widgetApi.receiveRoomEvents('m.room.message', {limit: limit, since: fullyRead});
+        } else {
+            roomEvents = await widgetApi.receiveRoomEvents('m.room.message', {limit: limit});
+        }
+
+        const messages = processMessages(roomEvents);
+        const displayNameData = await getDisplayNameData(widgetApi);
+        return {messages, displayNameData}
+    }
+
+    useEffect(() => {
+        summarize(true);
+    }, []);
+
+    function summarize(useUnread: boolean, limit?: number) {
+        setLoading(true);
+        setNoMessages(false);
+
+        fetchData(useUnread, limit).then(({messages, displayNameData}) => {
+            if (messages.length === 0) {
+                setNoMessages(true);
+                setLoading(false);
+            } else {
+                setMessageCount(messages.length);
+                complete(generatePrompt(messages, displayNameData));
+            }
+        })
+    }
+
+    if (error) {
+        return <p>{ error }</p>
+    }
+
+    return (
+        <div className="mt-5 p-3">
+            { noMessages && (
+                <>
+                    <p className="font-medium font-sans text-gray-600">No new messages.</p>
+                    <p className="font-bold font-sans text-xl mt-1">Summarize recent messages?</p>
+                    <input className="block border-2 border-gray-200 px-2 py-2 w-20 mt-4 rounded-lg focus:ring focus:outline-none ring-gray-300 text-black" type="number" step="1" min="1" max="1000" onChange={(e) => setCount(parseInt(e.target.value))} value={count}/>
+                    <button className="block rounded-lg px-10 py-3 text-center bg-black text-white mt-6 transition duration-300 hover:bg-gray-800" onClick={() => summarize(false, count)}>Summarize</button>
+                </>
+            )}
+
+            { loading && (
+                <>
+                    <p>Computing...</p>
+                </>
+            )}
+
+            { summarizing && (
+                <>
+                    <p className="font-semibold font-sans">{messageCount} messages:</p>
+                    <pre className="max-w-full whitespace-pre-wrap font-sans">{completion}</pre>
+                </>
+            )}
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,32 @@
 "use client"
 
-import dynamic from "next/dynamic";
-// TODO: this should be imported client-side. Breaks builds sometimes
-import {WidgetApi} from '@beeper/matrix-widget-toolkit-api';
-import {ReactElement} from "react";
-import WidgetPage from "@/app/components/page"
-import {WidgetApiImpl} from '@beeper/matrix-widget-toolkit-api';
-
-const MuiThemeProvider = dynamic(() => import('@beeper/matrix-widget-toolkit-mui').then((mod) => mod.MuiThemeProvider), {
-    ssr: false,
-})
-const MuiWidgetApiProvider = dynamic(() => import('@beeper/matrix-widget-toolkit-mui').then((mod) => mod.MuiWidgetApiProvider), {
-    ssr: false,
-})
-
-// Initiate the widget API on startup. The Client will initiate
-// the connection with `capabilities` and we need to make sure
-// that the message doesn't get lost while we are initiating React.
-const widgetApiPromise = WidgetApiImpl.create({
-    // Tell which capabilities should be requested at startup. One
-    // can also request capabilities after the application started.
-    capabilities: [
-        // WidgetEventCapability.forStateEvent(
-        //     EventDirection.Receive,
-        //     STATE_EVENT_ROOM_NAME
-        // ),
-    ],
-});
-
-
-function App({
-                 widgetApiPromise,
-             }: {
-    widgetApiPromise: Promise<WidgetApi>;
-}): ReactElement {
-    return (
-        <MuiThemeProvider>
-            {/* Fallback suspense if no higher one is registered (used for i18n) */}
-            <MuiWidgetApiProvider
-                widgetApiPromise={widgetApiPromise}
-                widgetRegistration={{
-                    name: 'Summarizer'
-                }}
-            >
-                <WidgetPage />
-            </MuiWidgetApiProvider>
-        </MuiThemeProvider>
-    );
-}
-
+import { EventDirection, WidgetEventCapability } from '@beeper/matrix-widget-api';
+import { MuiCapabilitiesGuard } from "@beeper/matrix-widget-toolkit-mui";
+import Content from "@/app/content";
 
 export default function Home() {
+
+
     return (
-        <App widgetApiPromise={widgetApiPromise}/>
-    )
-}
+        <>
+            <MuiCapabilitiesGuard
+                capabilities={[
+                    WidgetEventCapability.forStateEvent(
+                        EventDirection.Receive,
+                        'm.room.member'
+                    ),
+                    WidgetEventCapability.forRoomEvent(
+                        EventDirection.Receive,
+                        'm.room.message'
+                    ),
+                    WidgetEventCapability.forRoomAccountData(
+                        EventDirection.Receive,
+                        'm.fully_read'
+                    ),
+                ]}
+            >
+                <Content />
+            </MuiCapabilitiesGuard>
+        </>
+    );
+};
